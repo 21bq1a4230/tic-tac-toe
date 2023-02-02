740f4e67c63db0356a79c3b91530d65e2016c906
21bq1a4230 <21bq1a4230@vvit.net>
2023-02-02 20:58:47 +0530

    modified for two player mode may add vs computer in future

--- op.js
+++ op.js
@@ -1,4 +1,4 @@
-var playerCounter = 0;
+var turn = 0;
 c11 = document.getElementById("c11");
 c12 = document.getElementById("c12");
 c13 = document.getElementById("c13");
@@ -8,113 +8,126 @@ c23 = document.getElementById("c23");
 c31 = document.getElementById("c31");
 c32 = document.getElementById("c32");
 c33 = document.getElementById("c33");
-c11.addEventListener("click", makeMove);
 c12.addEventListener("click", makeMove);
 c13.addEventListener("click", makeMove);
 c21.addEventListener("click", makeMove);
 c22.addEventListener("click", makeMove);
 c23.addEventListener("click", makeMove);
+c11.addEventListener("click", makeMove);
 c31.addEventListener("click", makeMove);
 c32.addEventListener("click", makeMove);
 c33.addEventListener("click", makeMove);
-var b = document.querySelectorAll(".comp");
-// var forx = "<h1 style='text-align: center'>X<h1>"
-// var foro = "<h1 style='text-align: center'>O<h1>"
-var forx = "X";
-var foro = "O";
-function getIndex(c){
-    for(var i=0;i<b.length;i++){
-        if(c === b[i]){
-            return i+1
+var one = "X";
+var two = "O";
+var x = 1;
+var o = 2;
+var over = "over";
+var game = "";
+function makeMove(e){
+    tar = e.target;
+    var barr = Array.from(b);
+    var ind = barr.indexOf(tar);
+    if(game != over){
+        if(turn%2===0){
+            tar.innerHTML = one;
+            turn++;
+            checkForWinner(one ,e);
+            
+        }
+        else{
+            tar.innerHTML = two;
+            turn++;
+            checkForWinner(two , e);
         }
     }
 }
-function makeMove(event){
-    if(playerCounter%2 === 0){
-        event.target.innerHTML= forx;
-        playerCounter += 1;
-        console.log(checkForWinner(event,forx));
-        console.log(b);
+var b = document.querySelectorAll(".comp");
+function checkForWinner(p,e){
+    var lt = [];
+    var boardarr = [];
+    var barr = Array.from(b);
+    for(var i=0;i<9;i++){
+        lt.push(b[i]);
+        if((i+1)%3===0){
+            boardarr.push(lt);
+            lt = [];
+        }
+    }
+    var i=0,j=0;
+    console.log(barr);
+    var ind = barr.indexOf(e.target);
+    if(ind < 3){
+        i = 0;
+        j = ind;
+    }
+    else if(ind>=3 && ind < 6){
+        i = 1;
+        j = ind - 3;
+    }
+    else if(ind>=6 && ind < 9){
+        i = 2;
+        j = ind - 6;
+    }
+    if(rowCrossed(i,j,boardarr,p) || columnCrossed(i,j,boardarr,p) || diagonalCrossed(i,j,boardarr,p)){
+        gameOver(e,p);
+    }
+    // else if(tiehappend()){
+    //     gameTied();
+    // }
+}
+function rowCrossed(i,j,array,turn){
+    if(array[i][0].innerHTML === turn && array[i][1].innerHTML === turn && array[i][2].innerHTML === turn){
+        return true;
     }
     else{
-        event.target.innerHTML = foro;
-        playerCounter += 1;
-        console.log(checkForWinner(event,foro));
+        return false;
     }
 }
-function getCoordinates(ele){
-
-    var a = getIndex(ele);
-    var r,c;
-    if(a<=3){
-        r = 1;
-        c = a;
+function columnCrossed(i,j,array,turn){
+    if(array[0][j].innerHTML === turn && array[1][j].innerHTML === turn && array[2][j].innerHTML === turn){
+        return true;
     }
-    else if(3<a && a<=6){
-        r = 2;
-        c = a-3;
+    else{
+        return false;
     }
-    else if(6<a && a<=9){
-        r = 3;
-        c = a-6;
-    }
-    return [r-1,c-1];
 }
-function checkForWinner(e,p){
-    var arr = [];
-    var sarr = [];
-    for(var i=0;i<9;i++){
-        sarr.push(b[i].innerHTML);
-        if((i+1)%3===0){
-            arr.push(sarr);
-            sarr = [];
-        }
-    }
-    console.log(arr);
-    var coord = getCoordinates(e.target);
-    var r = coord[0];
-    var c = coord[1];
-    var count=0;
-    console.log(r,c,arr[r][c])
-    //checking the horizontal row in which the element is presented
-    for(var i=0;i<3;i++){
-        if(arr[r][i] === p){
-            count += 1;
-        }
-    }
-    if(count === 3){
-        return "winner"+" "+p;
-    }
-    //checking the vertical row in which the element is presented
-    for(var i=0;i<3;i++){
-        if(arr[i][c] === p){
-            count+= 1;
-        }
-    }
-    if(count === 3){
-        return "winner"+" "+p;
-    }
-    //checking the diagonal rows in which the element is presented.
-    if(r===c){
+function diagonalCrossed(i,j,array,turn){
+    if(i===j){
         for(var i=0;i<3;i++){
-            if(arr[i][i] === p){
-                count+= 1
+            if(array[i][i].innerHTML !== turn){
+                return false;
             }
         }
-        if(count === 3){
-            return "winner"+" "+p;
-        }
+        return true;
     }
-    else if(r+c == 2){
+    if(i+j === 2){
         for(var i=0;i<3;i++){
             for(var j=0;j<3;j++){
-                if(i+j === 2 && arr[i][j] === p){
-                    count += 1
+                if(i+j === 2 && array[i][j].innerHTML !== turn){
+                    return false;
                 }
             }
         }
-        if(count === 3){
-            return "winner"+" "+p;
-        }
+        return true;
     }
+}
+// function tiehappend(array){
+//     for(var i=0;i<3;i++){
+//         for(var j=0;j<3;j++){
+//             if(array[i][j].innerHTML !== one || array[i][j].innerHTML !== two){
+//                 return false;
+//             }   
+//         }
+//     }
+//     return true;
+// }
+function gameOver(event,turn){
+    console.log("player "+turn+" won");
+    game = over;
+    var win = document.getElementById("win");
+    console.log(win);
+    win.innerHTML = "<h1> player "+turn+" WON !!!<br> CONGRATULATIONS</h1>"
+}
+function gameTied(event){
+    console.log("game not tied");
 }
